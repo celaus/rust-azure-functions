83b769b60892387967d1a9d0c4d89031a576697a
Claus Matzinger <claus.matzinger+kb@gmail.com>
2018-04-13 10:25:29 +0200

    fixup XVII

--- pibench-rs/index.js
+++ pibench-rs/index.js
@@ -1,4 +1,4 @@
-const rust = require("./pibench-rs/wasm_pibench.js"); 
+const rust = require("pibench-rs\wasm_pibench.js"); 
 
 module.exports = function (context, req) {
     context.log(process.versions);
