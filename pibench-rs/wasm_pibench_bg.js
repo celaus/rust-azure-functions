3280cab352d31bac4331046feacc91feea604565
Claus Matzinger <claus.matzinger+kb@gmail.com>
2018-04-22 08:48:32 +0200

    updated path II

--- pibench-rs/wasm_pibench_bg.js
+++ pibench-rs/wasm_pibench_bg.js
@@ -1,7 +1,7 @@
 let imports = {};
 imports['./wasm_pibench'] = require('./wasm_pibench');
 
-const bytes = require('fs').readFileSync('./wasm_pibench_bg.wasm');
+const bytes = require('fs').readFileSync('./pibench-rs/wasm_pibench_bg.wasm');
 const wasmModule = new WebAssembly.Module(bytes);
 const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
 module.exports = wasmInstance.exports;
